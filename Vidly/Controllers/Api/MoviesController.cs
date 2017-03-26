ff3c307831b68faae82769272d096bb3c7eb8b61
Leskyam <leskyam@gmail.com>
2017-03-25 22:09:29 -0500

    Implement Actions for MoviesController (API)
    - Implement mapper based on Interface

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -17,34 +18,67 @@ namespace Vidly.Controllers.Api
         // GET /api/movies
         public IHttpActionResult GetMovies()
         {
-            throw new NotImplementedException();
+            var moviesDto = new List<MovieDto>();
+            foreach (var movie in _db.Movies)
+            {
+                var movieDto = new MovieDto();
+                MovieDto.Mapper(movie, movieDto);
+                moviesDto.Add(movieDto);
+                Debug.WriteLine($"{movieDto.Id}: {movieDto.Name}");
+            }
+            return Ok(moviesDto);
         }
 
         // Get /api/movies/1
         public IHttpActionResult GetMovie(int id)
         {
-            throw new NotImplementedException();
+            var movieDto = MovieDto.Mapper(_db.Movies.SingleOrDefault(m => m.Id == id), new MovieDto());
+            if(movieDto == null) 
+                return NotFound();
+
+            return Ok(movieDto);
         }
 
         // POST /api/movies/
         [System.Web.Mvc.HttpPost]
         public IHttpActionResult PostMovie(MovieDto movieDto)
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid) return BadRequest();
+
+            movieDto.Id = _db.Movies.Add((Movie)MovieDto.Mapper(movieDto, new Movie())).Id;
+            _db.SaveChanges();
+
+            return Created($"{Request.RequestUri}/{movieDto.Id}", movieDto);
         }
 
         // PUT /api/movies/
         [System.Web.Mvc.HttpPut]
-        public IHttpActionResult UpdateMovie(MovieDto movieDto)
+        public IHttpActionResult PutMovie(MovieDto movieDto)
         {
-            throw new NotImplementedException();
+            if (!ModelState.IsValid) return BadRequest();
+
+            var dbMovie = _db.Movies.SingleOrDefault(m => m.Id == movieDto.Id);
+            if (dbMovie == null)
+                return NotFound();
+
+            MovieDto.Mapper(movieDto, dbMovie);
+            _db.SaveChanges();
+
+            return Ok(movieDto);
+
         }
 
         // DELETE /api/movies/
         [System.Web.Mvc.HttpDelete]
         public IHttpActionResult DeleteMovie(int id)
         {
-            throw new NotImplementedException();
+            var dbMovie = _db.Movies.SingleOrDefault(m => m.Id == id);
+            if (dbMovie == null) return BadRequest();
+
+            _db.Movies.Remove(dbMovie);
+            _db.SaveChanges();
+
+            return Ok(true);
         }
 
 
--- Vidly/DTOs/MovieDto.cs
+++ Vidly/DTOs/MovieDto.cs
@@ -1,15 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Web;
+using Vidly.Models;
 
 namespace Vidly.DTOs
 {
-    public class MovieDto
+    public class MovieDto : IMovie
     {
-        public MovieDto()
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        public int InStock { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        public static IMovie Mapper(IMovie from, IMovie to)
         {
-            throw new NotImplementedException();
+            if (from == null) return null;
+
+            foreach (var fromProperty in typeof(IMovie).GetProperties())
+            {
+                var toProperty = to.GetType().GetProperty(fromProperty.Name);
+                toProperty.SetValue(to, fromProperty.GetValue(from));
+            }
+            return to;
         }
+
+    }
+
+    public interface IMovie
+    {
+        int Id { get; set; }
+
+        [Required]
+        string Name { get; set; }
+
+        [Required]
+        DateTime ReleaseDate { get; set; }
+
+        [Required]
+        DateTime DateAdded { get; set; }
+
+        int InStock { get; set; }
+
+        [Required]
+        int GenreId { get; set; }
+
+
     }
 }
